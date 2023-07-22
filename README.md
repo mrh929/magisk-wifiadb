e2927c59356681d26d36a2964b2e6ac2c815a4f5
809999463@qq.com <809999463@qq.com>
2023-07-23 03:09:45 +0800

    Add feature 1. store logs 2. configure adb port in config file

--- README.md
+++ README.md
@@ -1,16 +1,39 @@
-# Magisk
+# Magisk-WiFiADB
 
 A Magisk module that enables continuous availability of WiFi ADB service without requiring any Android app.
+
+## Feature
+
+- Ensure WiFi ADB always available
+
+- Disable WiFi ADB in real time
+  
+  If the module is started on boot, **clicking the button** to open the module and close the module in `Magisk Manager` will directly **modify the status** of the WiFi ADB.
 
 ## Usage
 
 Enable this module in magisk.
 
+### Configuration
+
+You can configurure module setting by creating a `config` file in `module root dir` (usually in `/data/adb/modules/magisk-wifiadb`).
+
+And write the following variables:
+
+```
+ADB_PORT=[YOUR_ADB_PORT]
+ENABLE_LOG=[1 (for enable) / 0 (for disable)]
+```
+
+### Logs
+
+If you set `ENABLE_LOG=1`, the logs will be saved in `/data/local/tmp/wifiadb.log`.
+
 ## How Does It Work
 
 By setting properties in Android to enable WiFi ADB:
 
-``` shell
+```shell
 su
 setprop service.adb.tcp.port 5555
 stop adbd
--- common/service.sh
+++ common/service.sh
@@ -8,6 +8,10 @@ MODDIR=${0%/*}
 
 # functions
 print_log() {
+  if [ "$ENABLE_LOG" != "1" ]; then
+    return
+  fi
+
   log_text="$1"
   now=$(date '+[%Y-%m-%d %I:%M:%S]')
   echo "$now $log_text" >>/data/local/tmp/wifiadb.log
@@ -40,19 +44,41 @@ maintain_adb_availability() {
     is_module_enable
     if [ $? -eq 1 ]; then
       print_log "module is enabled"
-      if [ "$(getprop service.adb.tcp.port)" != "5555" ]; then
-        print_log "======= adb is not started, starting adb ======="
-        start_adb 5555
+      if [ "$(getprop service.adb.tcp.port)" != "$ADB_PORT" ]; then
+        print_log "adb is not started, starting adb"
+        start_adb "$ADB_PORT"
       fi
     else
       print_log "module is disabled"
       if [ "$(getprop service.adb.tcp.port)" != "" ]; then
-        print_log "======= adb is not stopped, stopping adb ======="
+        print_log "adb is not stopped, stopping adb"
         stop_adb
       fi
     fi
     sleep 5
   done
+}
+
+load_config() {
+  CONFIG_PATH="${MODDIR}/config"
+
+  if [[ -f "$CONFIG_PATH" ]]; then
+    source "$CONFIG_PATH"
+    print_log "config file loaded."
+  else
+    print_log "config file not found."
+  fi
+}
+
+parse_config() {
+  ADB_PORT_PATTERN='^([1-9]|[1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$'
+
+  if echo "$ADB_PORT" | grep -Eq "$ADB_PORT_PATTERN"; then
+    print_log "ADB_PORT value: $ADB_PORT"
+  else
+    ADB_PORT="5555"
+    print_log "ADB_PORT parse failed, set to default value: $ADB_PORT"
+  fi
 }
 
 # This script will be executed in late_start service mode
@@ -61,6 +87,7 @@ maintain_adb_availability() {
     sleep 5
   done
 
-  print_log "boot complete"
+  load_config
+  parse_config
   maintain_adb_availability
 ) &
