6b084793be0a7ed68b11e0303756a31e63d4d91c
809999463@qq.com <809999463@qq.com>
2023-07-23 01:32:56 +0800

    Add features: 1. Log will be saved in /data/local/tmp/wifiadb.log 2. Magisk manager can control wifi adb status now (only when magisk-wifiadb is enabled at boot time).

--- common/service.sh
+++ common/service.sh
@@ -6,21 +6,61 @@
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
+# functions
+print_log() {
+  log_text="$1"
+  now=$(date '+[%Y-%m-%d %I:%M:%S]')
+  echo "$now $log_text" >>/data/local/tmp/wifiadb.log
+}
+
+is_module_enable() {
+  if [ -e "${MODDIR}/disable" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+start_adb() {
+  port=$1
+  setprop service.adb.tcp.port "$port"
+  stop adbd
+  start adbd
+}
+
+stop_adb() {
+  setprop service.adb.tcp.port ""
+  stop adbd
+  start adbd
+}
+
+maintain_adb_availability() {
+  while true; do
+    print_log "check module status"
+    is_module_enable
+    if [ $? -eq 1 ]; then
+      print_log "module is enabled"
+      if [ "$(getprop service.adb.tcp.port)" != "5555" ]; then
+        print_log "======= adb is not started, starting adb ======="
+        start_adb 5555
+      fi
+    else
+      print_log "module is disabled"
+      if [ "$(getprop service.adb.tcp.port)" != "" ]; then
+        print_log "======= adb is not stopped, stopping adb ======="
+        stop_adb
+      fi
+    fi
+    sleep 5
+  done
+}
+
 # This script will be executed in late_start service mode
 (
   until [ "$(getprop sys.boot_completed)" -eq 1 ]; do
     sleep 5
   done
 
-  while true; do
-
-    while [ "$(getprop service.adb.tcp.port)" = "5555" ]; do
-      sleep 5
-    done
-
-    setprop service.adb.tcp.port 5555
-    stop adbd
-    start adbd
-
-  done
+  print_log "boot complete"
+  maintain_adb_availability
 ) &
